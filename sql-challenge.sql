f22d5c35cbf460700e3453e63ceb2e84040b6675
Yeldana Sadykova <yeldana.sadykova@gmail.com>
2023-08-03 00:50:45 +0200

    add general challenge task

--- sql-challenge.sql
+++ sql-challenge.sql
@@ -46,3 +46,31 @@ LIMIT 5;
 SELECT count (title) FROM film
 WHERE length <= 50;
 
+-- How many payment transactions were greater than $5.00?
+
+SELECT count(amount) FROM payment
+WHERE amount > 5;
+
+-- How many actors have a first name that starts with the letter P?
+
+SELECT count(*) FROM actor
+WHERE first_name LIKE 'P%';
+
+-- How many unique districts are our customers from?
+
+SELECT COUNT(DISTINCT(district)) FROM address;
+
+-- Retrieve the list of names for those distinct districts from the previous question.
+
+SELECT DISTINCT(district) FROM address;
+
+-- How many films have a rating of R and a replacement cost between $5 and $15?
+
+SELECT count(*) FROM film
+WHERE rating ='R'
+AND replacement_cost BETWEEN 5 AND 15;
+
+-- How many films have the word Truman somewhere in the title?
+
+SELECT count(*) FROM film
+WHERE title LIKE '%Truman%';
