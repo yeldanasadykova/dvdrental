ba5dc45abb9f02e6ab19af6deaa06f4eb5e5d057
Yeldana Sadykova <yeldana.sadykova@gmail.com>
2023-08-02 23:13:43 +0200

    add challenge ORDER BY

--- sql-challenge.sql
+++ sql-challenge.sql
@@ -26,3 +26,23 @@ WHERE title ='Outlaw Hanky';
 SELECT phone FROM address
 WHERE address ='259 Ipoh Drive';
 
+/* We want to reward our first 10 paying customers.
+   What are the customer ids of the first 10 customers who created a payment? */
+
+SELECT customer_id FROM payment
+ORDER BY payment_date ASC
+LIMIT 10;
+
+/* A customer wants to quickly rent a video to watch over their short lunch break.
+   What are the titles of the 5 shortest (in length of runtime) movies? */
+   
+SELECT title,length FROM film
+ORDER BY length ASC
+LIMIT 5;
+
+/* If the previous customer can watch any movie that is 50 minutes 
+   or less in run time, how many options does she have? */
+   
+SELECT count (title) FROM film
+WHERE length <= 50;
+
