7508218130d8b460bbd32d2e7df2ba139f49fe29
Yeldana Sadykova <yeldana.sadykova@gmail.com>
2023-08-07 23:47:24 +0200

    add new challenge

--- sql-challenge.sql
+++ sql-challenge.sql
@@ -117,3 +117,26 @@ WHERE staff_id = 2
 GROUP BY customer_id,staff_id
 HAVING SUM(amount) > 100
 ORDER BY customer_id;
+
+/* Returns the customers IDs of customer who have spent at least $110 with the
+   staff member who has an ID of 2. */
+   
+SELECT customer_id FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id,staff_id
+HAVING SUM(amount) > 110
+ORDER BY customer_id;
+
+-- How many films begin with the letter J?
+
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'J%';
+
+/* What customer has the highest customer ID number whose name starts 
+   with an 'E' and has an address ID lower than 500? */
+   
+SELECT first_name,last_name,address_id FROM customer
+WHERE first_name LIKE 'E%' 
+AND address_id < 500
+ORDER BY address_id DESC
+LIMIT 1;
